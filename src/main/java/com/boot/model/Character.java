23eda7bfaa7df8912126a15cc477897a3fccc4a1
kheslop <kheslop@mastercontrol.com>
2019-10-28 09:36:06 -0600

    changed some headers in _form.html added computers field to character_view.html added setters / getters for skill ranks in Character model

--- src/main/java/com/boot/model/Character.java
+++ src/main/java/com/boot/model/Character.java
@@ -114,7 +114,6 @@ public class Character {
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
@@ -122,87 +121,93 @@ public class Character {
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getCharacterClassType() {
 		return characterClassType;
 	}
-
 	public void setCharacterClassType(String description) {
 		this.characterClassType = description;
 	}
-
 	public String getRace() {
 		return race;
 	}
-
 	public void setRace(String condition) {
 		this.race = condition;
 	}
-
 	public String getTheme() { return theme; }
-
 	public void setTheme(String theme) { this.theme = theme; }
-
 	public Integer getLevel() { return level; }
-
 	public void setLevel(Integer level) { this.level = level; }
-
 	public String getSize() { return size; }
-
 	public void setSize(String size) { this.size = size; }
-
 	public String getGender() { return gender; }
-
 	public void setGender(String gender) { this.gender = gender; }
-
 	public String getHomeWorld() { return homeWorld; }
-
 	public void setHomeWorld(String homeWorld) { this.homeWorld = homeWorld; }
-
 	public Integer getSpeed() { return speed; }
-
 	public void setSpeed(Integer speed) { this.speed = speed; }
-
 	public String getAlignment() { return alignment; }
-
 	public void setAlignment(String alignment) { this.alignment = alignment; }
-
 	public String getDeity() { return deity; }
-
 	public void setDeity(String deity) { this.deity = deity; }
 
 	public Integer getDexterity() { return strength; }
-
 	public void setDexterity(Integer dexterity) { this.dexterity = dexterity; }
-
 	public Integer getConstitution() { return constitution; }
-
 	public void setConstitution(Integer constitution) { this.constitution = constitution; }
-
 	public Integer getIntelligence() { return intelligence; }
-
 	public void setIntelligence(Integer intelligence) { this.intelligence = intelligence; }
-
 	public Integer getWisdom() { return wisdom; }
-
 	public void setWisdom(Integer wisdom) { this.wisdom = wisdom; }
-
 	public Integer getCharisma() { return charisma; }
-
 	public void setCharisma(Integer charisma) { this.charisma = charisma; }
-
 	public Integer getStrength() { return strength; }
-
 	public void setStrength(Integer strength) { this.strength = strength; }
-
 	public String getNotes() { return notes; }
-
 	public void setNotes(String notes) { this.notes = notes; }
 
-	// TODO: Add skills to section
+	public Integer getAcrobatics() { return acrobatics; }
+	public void setAcrobatics() { this.acrobatics = acrobatics; }
+	public Integer getAthletics() { return athletics; }
+	public void setAthletics() { this.athletics = athletics; }
+	public Integer getBluff() { return bluff; }
+	public void setBluff() { this.bluff = bluff; }
+	public Integer getComputers() { return computers; }
+	public void setComputers() { this.computers = computers; }
+	public Integer getCulture() { return culture; }
+	public void setCulture() { this.culture = culture; }
+	public Integer getDiplomacy() { return diplomacy; }
+	public void setDiplomacy() { this.diplomacy = diplomacy; }
+	public Integer getDisguise() { return disguise; }
+	public void setDisguise() { this.disguise = disguise; }
+	public Integer getEngineering() { return engineering ; }
+	public void setEngineering() { this.engineering = engineering; }
+	public Integer getIntimidate() { return intimidate; }
+	public void setIntimidate() { this.intimidate = intimidate;}
+	public Integer getLifeScience() { return lifeScience; }
+	public void setLifeScience() { this.lifeScience = lifeScience;}
+	public Integer getMedicine() { return medicine; }
+	public void setMedicine() { this.medicine = medicine;}
+	public Integer getMysticism() { return mysticism; }
+	public void setMysticism() { this.mysticism = mysticism;}
+	public Integer getPerception() { return perception; }
+	public void setPerception() { this.perception = perception;}
+	public Integer getPhysicalScience() { return physicalScience; }
+	public void setPhysicalScience() { this.physicalScience = physicalScience;}
+	public Integer getPiloting() { return piloting; }
+	public void setPiloting() { this.piloting = piloting;}
+	public Integer getProfession() { return profession; }
+	public void setProfession() { this.profession = profession;}
+	public Integer getSenseMotive() { return senseMotive; }
+	public void setSenseMotive() { this.senseMotive = senseMotive;}
+	public Integer getSleightOfHand() { return sleightOfHand; }
+	public void setSleightOfHand() { this.sleightOfHand = sleightOfHand;}
+	public Integer getStealth() { return stealth; }
+	public void setStealth() { this.stealth = stealth;}
+	public Integer getSurvival() { return survival; }
+	public void setSurvival() { this.survival = survival;}
+
 
 }
